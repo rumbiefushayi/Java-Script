a7fa9ef5db072f5a0ff9ddc064b25c6a3046ca46
rumbiefushayi <47625143+rumbiefushayi@users.noreply.github.com>
2019-02-28 10:58:09 +0200

    Javascript Numbers
    calculated variables using an external js file

---
+++ maths.js
@@ -0,0 +1,12 @@
+var x =5;
+var y =6;
+
+var a =x+y;
+var b =y-x;
+var c = x*y;
+var d = x/y;
+document.getElementById("demo").innerHTML=
+ "the value of a is;" + a + "<br/>" +
+ "the value of b  is;" + b + "<br/>"+
+ "the value of c is;" + c + "<br/>"+
+ "the value of d is;" + d ;
