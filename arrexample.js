3ae4e057e9dd6ac3b57980201028d1c98d5804ff
rumbiefushayi <47625143+rumbiefushayi@users.noreply.github.com>
2019-03-04 11:11:38 +0200

    Java functions
    Used the push function to add "twelve" to the array

--- arrexample.js
+++ arrexample.js
@@ -1,4 +1,5 @@
   var things = ["23", "Cars", "Harvey", "11"]
+  things.push("twelve"); 
 
   document.getElementById('things').innerHTML= "<ul>" + "<li>" things[2] "</li>" + "<li>" "has" </li> + 
-                                                "<li>" (things[0]-things[3]) + "</li>"  "<li>"  things[1]  "</li>" + "</u>"
+                                                "<li>" + things[4] + (things[0]-things[3]) + "</li>"  "<li>"  things[1]  "</li>" + "</u>"
