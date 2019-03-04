c57cd0e62f21537f7b20783c1e1c8a29b5dab5ee
rumbiefushayi <47625143+rumbiefushayi@users.noreply.github.com>
2019-03-04 10:24:17 +0200

    Java Arrays
    Declared an array in Javascript using
    1. document.getElementById
    2. document.write to display data.

---
+++ arrexample.js
@@ -0,0 +1,4 @@
+  var things = ["23", "Cars", "Harvey", "11"]
+
+  document.getElementById('things').innerHTML= "<ul>" + "<li>" things[2] "</li>" + "<li>" "has" </li> + 
+                                                "<li>" (things[0]-things[3]) + "</li>"  "<li>"  things[1]  "</li>" + "</u>"
