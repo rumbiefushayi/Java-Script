1a1f065f254ac1bdd005a429d48a2cd5673ba40d
rumbiefushayi <47625143+rumbiefushayi@users.noreply.github.com>
2019-03-04 13:04:24 +0200

    Java Functions
    Used
    a) Splice function  to
    1) add the element Altezza at two different positions in the array cars.
    2) to delete other after element
    b) Slice function to create a new array out of an existing

--- arrexample.js
+++ arrexample.js
@@ -1,5 +1,5 @@
   var things = ["23", "Cars", "Harvey", "11"]
-  things.push("twelve"); 
+  things.push("twelve");
 
   document.getElementById('things').innerHTML= "<ul>" + "<li>" things[2] "</li>" + "<li>" "has" </li> + 
                                                 "<li>" + things[4] + (things[0]-things[3]) + "</li>"  "<li>"  things[1]  "</li>" + "</u>"
