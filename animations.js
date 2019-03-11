9b1a9dc7c81e60c70b5e7439490c704ac14b2d11
rumbiefushayi <47625143+rumbiefushayi@users.noreply.github.com>
2019-03-11 10:09:43 +0200

    Javascript Events
    Added more javascript event to a code showing a panel

--- animations.js
+++ animations.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
   	$("#flip").click(function(){
-  		$("#panel").slideToggle("slow");
- 	 });
+  	    $("#panel").slideToggle()
+  	        $("#panel").css("background-color", "#FF1493")
+  	            alert("Hello! I am an alert box!")
+  	            	$("#panel").text("Click me and see")
+  	                	$("#panel").html("<b>Hello World!</b>");
+    });
 });
+
+
+
