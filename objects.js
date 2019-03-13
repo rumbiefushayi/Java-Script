993c9597d2297340a1133d64efb880b90d71ce84
rumbiefushayi <47625143+rumbiefushayi@users.noreply.github.com>
2019-03-13 12:17:50 +0200

    J-Query
    used j query elements and events in a loop.

---
+++ objects.js
@@ -0,0 +1,13 @@
+
+    $(document).ready(function(){
+      	$("button.guy").click(function(){
+          	$("p").fadeIn(3000);
+      	});
+    });          
+    
+    $(document).ready(function(){
+      	$("button.girl").click(function(){
+        	$("p").fadeOut(3000);
+      	});
+    });          
+    
